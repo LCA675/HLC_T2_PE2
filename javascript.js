10cbf8fdb694ef3f9f282232329a33a00f033120
LCA675 <lauracalleanguita@gmail.com>
2024-12-13 13:03:23 +0100

    :,,,)

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function getRandomBotMessage () {
+    const messages = [
+        "Creo que no.",
+        "¡Totalmente!",
+        "Eso suena interesante.",
+        "No estoy seguro de entender, ¿puedes explicar más?",
+        "¡Qué genial!",
+        "Hmm, déjame pensar...",
+        "¡Por supuesto!",
+        "No sé, pero suena divertido.",
+        "¿Puedes decirme más?",
+        "Eso es un misterio para mí.",
+        "¡Me encanta hablar contigo!"]
+
+    return messages[Math.floor(Math.random() * messages.length)];
+}
+
